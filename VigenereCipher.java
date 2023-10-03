0b0fd63a799c24a4759e447cc9d43d68db2f8e50
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-10-02 23:28:30 -0500

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,68 +1,78 @@
 public class VigenereCipher {
 
-  public static String encode(String plaintext, String key) {
-    StringBuilder encodedText = new StringBuilder(); // Instantiate a StringBuilder
+    // Method to encode a plaintext using Vigenere cipher
+    public static String encode(String plaintext, String key) {
+        // If the key is empty, return the original plaintext
+        if (key.isEmpty()) {
+            return plaintext;
+        }
 
-    for (int i = 0, j = 0; i < plaintext.length(); i++) { // Loop through the input text & key
-      char c = plaintext.charAt(i); // Set char c as the index of the input plaintext string
-      char keyChar = key.charAt(j % key.length()); // Check if the key is in a valid unicode & lowercase
+        StringBuilder encodedText = new StringBuilder(); // Instantiate a StringBuilder
 
-      int encodedValue = ((c + keyChar - 2 * 'a') % 26) + 'a'; // Set and check char as a valid unicode value
-      encodedText.append((char) encodedValue); // Append the unicoded value to the StringBuilder
-      j++; // Increment the index of the key within the loop
-    }
-    return encodedText.toString(); // Return using the string conversion function
-  }
+        for (int i = 0, j = 0; i < plaintext.length(); i++) { // Loop through the input text & key
+            char c = plaintext.charAt(i); // Set char c as the index of the input plaintext string
+            char keyChar = key.charAt(j % key.length()); // Check if the key is in a valid unicode & lowercase
 
-  public static String decode(String encodedText, String key) {
-    StringBuilder decodedText = new StringBuilder(); // Following the same process as encode method
-
-    for (int i = 0, j = 0; i < encodedText.length(); i++) {
-      char c = encodedText.charAt(i);
-      char keyChar = key.charAt(j % key.length());
-
-      int decodedValue = ((c - keyChar + 26) % 26) + 'a'; // Change calculation to set and check char for a decoded unicode value
-      decodedText.append((char) decodedValue);
-      j++;
-    }
-    return decodedText.toString();
-  }
-
-  public static boolean validateInput(String[] args) {
-    if (args.length != 3) { // Checking for whitespaces
-      System.out.println("Invalid input. Please use a correct number of args.");
-      return false;
+            int encodedValue = ((c + keyChar - 2 * 'a') % 26) + 'a'; // Set and check char as a valid unicode value
+            encodedText.append((char) encodedValue); // Append the unicoded value to the StringBuilder
+            j++; // Increment the index of the key within the loop
+        }
+        return encodedText.toString(); // Return using the string conversion function
     }
 
-    String instruction = args[0];
+    // Method to decode an encoded text using Vigenere cipher
+    public static String decode(String encodedText, String key) {
+        StringBuilder decodedText = new StringBuilder(); // Instantiate a StringBuilder
 
-    if (!instruction.equals("encode") && !instruction.equals("decode")) { // Checking for correct instructions
-      System.out.println("Invalid input. Please Use encode/decode.");
-      return false;
+        for (int i = 0, j = 0; i < encodedText.length(); i++) {
+            char c = encodedText.charAt(i);
+            char keyChar = key.charAt(j % key.length());
+
+            int decodedValue = ((c - keyChar + 26) % 26) + 'a'; // Change calculation to set and check char for a decoded unicode value
+            decodedText.append((char) decodedValue);
+            j++;
+        }
+        return decodedText.toString();
     }
 
-    return true;
-  }
+    // Method to validate input arguments
+    public static boolean validateInput(String[] args) {
+        if (args.length != 3) { // Checking for correct number of arguments
+            System.out.println("Invalid input. Please use a correct number of args.");
+            return false;
+        }
 
-  public static void processInput(String instruction, String text, String key) {
-    if (instruction.equals("encode")) {
-      String encodedText = encode(text, key);
-      System.out.println("Encoded Text: " + encodedText); // Process the input and return the encoded text
-    } else {
-      String decodedText = decode(text, key);
-      System.out.println("Decoded Text: " + decodedText); // Process the input and return the decoded text
+        String instruction = args[0];
+
+        if (!instruction.equals("encode") && !instruction.equals("decode")) { // Checking for correct instructions
+            System.out.println("Invalid input. Please Use encode/decode.");
+            return false;
+        }
+
+        return true;
     }
-  }
 
-  public static void main(String[] args) {
-    if (!validateInput(args)) {
-      return;
+    // Method to process input based on the instruction
+    public static void processInput(String instruction, String text, String key) {
+        if (instruction.equals("encode")) {
+            String encodedText = encode(text, key);
+            System.out.println("Encoded Text: " + encodedText); // Process the input and return the encoded text
+        } else {
+            String decodedText = decode(text, key);
+            System.out.println("Decoded Text: " + decodedText); // Process the input and return the decoded text
+        }
     }
 
-    String instruction = args[0]; // Initalize String args
-    String text = args[1];
-    String key = args[2];
+    // Main method to run the program
+    public static void main(String[] args) {
+        if (!validateInput(args)) {
+            return;
+        }
 
-    processInput(instruction, text, key); // Call processInput with initalized String args
-  }
+        String instruction = args[0]; // Initialize String args
+        String text = args[1];
+        String key = args[2];
+
+        processInput(instruction, text, key); // Call processInput with initialized String args
+    }
 }
