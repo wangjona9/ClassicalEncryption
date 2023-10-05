52b8f252e18c33fb87fb824ae60ebb051a531967
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-10-04 23:20:33 -0500

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -10,29 +10,32 @@ public class VigenereCipher {
 
     public static String cipher(String plaintext, String key, boolean isEncoding) {
         StringBuilder codedText = new StringBuilder(); // Instantiate a StringBuilder
-    
+
         for (int i = 0, j = 0; i < plaintext.length(); i++) { // Loop through result
             char c = plaintext.charAt(i); // Set char c as the index of the input plaintext string
-            char keyChar = (key.isEmpty()) ? 'a' : key.charAt(j % key.length()); // Check if the key is in a valid unicode & lowercase and not empty
-    
-            int modifier = isEncoding ? 1 : -1; // Boolean check to adjust calculation (postive for encode, negative for decode)
-            int transformedValue = ((((c - 'a') + modifier * (keyChar - 'a')) + 26) % 26) + 'a'; // Set and check char as a valid unicode value
-    
+            char keyChar = (key.isEmpty()) ? 'a' : key.charAt(j % key.length()); // Check if the key is in a valid
+                                                                                 // unicode & lowercase, and not empty
+
+            int modifier = isEncoding ? 1 : -1; // Boolean check to adjust calculation (postive for encode, negative for
+                                                // decode)
+            int transformedValue = ((((c - 'a') + modifier * (keyChar - 'a')) + 26) % 26) + 'a'; // Set and check char
+                                                                                                 // as a valid unicode
+                                                                                                 // value
+
             codedText.append((char) transformedValue); // Append the unicoded value to the StringBuilder
             j++; // Increment the index of the key within the loop
         }
-    
+
         return codedText.toString(); // Return using the string conversion function
     }
-    
 
-    public static String encode(String plaintext, String key) { 
+    public static String encode(String plaintext, String key) {
         return cipher(plaintext, key, true);
     } // encode
 
     public static String decode(String encodedText, String key) {
         return cipher(encodedText, key, false);
-    } // decode 
+    } // decode
 
     public static void main(String[] args) {
         if (args.length != 3) {
