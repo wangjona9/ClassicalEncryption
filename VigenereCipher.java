e21ad2c88e6cc720218c1b510eee486435c5e8c4
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-12 13:16:45 -0600

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,77 +1,75 @@
 /**
- * An implementation of the Vigenere
- * Cipher. Implements the command line
- * for user input.
- * 
+ * An implementation of the Vigenere Cipher. Implements the command line for user input.
+ *
  * @author Jonathan Wang
  * September 2023
  */
 public class VigenereCipher {
 
-    public static String cipher(String plaintext, String key, boolean isEncoding) {
-        StringBuilder codedText = new StringBuilder(); // Instantiate a StringBuilder
+  public static String cipher(String plaintext, String key, boolean isEncoding) {
+    StringBuilder codedText = new StringBuilder(); // Instantiate a StringBuilder
 
-        for (int i = 0, j = 0; i < plaintext.length(); i++) { // Loop through result
-            char c = plaintext.charAt(i); // Set char c as the index of the input plaintext string
-            char keyChar = (key.isEmpty()) ? 'a' : key.charAt(j % key.length()); // Check if the key is in a valid
-                                                                                 // unicode & lowercase, and not empty
+    for (int i = 0, j = 0; i < plaintext.length(); i++) { // Loop through result
+      char c = plaintext.charAt(i); // Set char c as the index of the input plaintext string
+      char keyChar = (key.isEmpty()) ? 'a' : key.charAt(j % key.length()); // Check if the key is in a valid
+                                                                             // unicode & lowercase, and not empty
 
-            int modifier = isEncoding ? 1 : -1; // Boolean check to adjust calculation (postive for encode, negative for
-                                                // decode)
-            int transformedValue = ((((c - 'a') + modifier * (keyChar - 'a')) + 26) % 26) + 'a'; // Set and check char
-                                                                                                 // as a valid unicode
-                                                                                                 // value
+      int modifier = isEncoding ? 1 : -1; // Boolean check to adjust calculation (positive for encode, negative for
+                                          // decode)
+      int transformedValue = ((((c - 'a') + modifier * (keyChar - 'a')) + 26) % 26) + 'a'; // Set and check char
+                                                                                           // as a valid unicode
+                                                                                           // value
 
-            codedText.append((char) transformedValue); // Append the unicoded value to the StringBuilder
-            j++; // Increment the index of the key within the loop
-        }
-
-        return codedText.toString(); // Return using the string conversion function
+      codedText.append((char) transformedValue); // Append the unicoded value to the StringBuilder
+      j++; // Increment the index of the key within the loop
     }
 
-    public static String encode(String plaintext, String key) {
-        return cipher(plaintext, key, true);
-    } // Encode
+    return codedText.toString(); // Return using the string conversion function
+  }
 
-    public static String decode(String encodedText, String key) {
-        return cipher(encodedText, key, false);
-    } // Decode
+  public static String encode(String plaintext, String key) {
+    return cipher(plaintext, key, true);
+  } // Encode
 
-    public static boolean validateInput(String[] args) {
-        if (args.length != 3) { 
-            System.out.println("Invalid input. Please use a correct number of args.");
-            return false;
-        } // Check for invalid number of args
- 
-        String instruction = args[0];
+  public static String decode(String encodedText, String key) {
+    return cipher(encodedText, key, false);
+  } // Decode
 
-        if (!instruction.equals("encode") && !instruction.equals("decode")) {
-            System.out.println("Invalid input. Please Use encode/decode."); 
-            return false;
-        } // Check for incorrect instruction call
+  public static boolean validateInput(String[] args) {
+    if (args.length != 3) {
+      System.out.println("Invalid input. Please use a correct number of args.");
+      return false;
+    } // Check for an invalid number of args
 
-        return true;
+    String instruction = args[0];
+
+    if (!instruction.equals("encode") && !instruction.equals("decode")) {
+      System.out.println("Invalid input. Please Use encode/decode.");
+      return false;
+    } // Check for an incorrect instruction call
+
+    return true;
+  }
+
+  public static void processInput(String instruction, String text, String key) {
+    if (instruction.equals("encode")) { // Print result to string for encode instruction
+      String encodedText = encode(text, key);
+      System.out.println("Encoded Text: " + encodedText);
+    } else {
+      String decodedText = decode(text, key); // Print result to string for decode instruction
+      System.out.println("Decoded Text: " + decodedText);
+    }
+  }
+
+  public static void main(String[] args) {
+    if (!validateInput(args)) {
+      return;
     }
 
-    public static void processInput(String instruction, String text, String key) {
-        if (instruction.equals("encode")) { // Print result to string for encode instruction
-            String encodedText = encode(text, key);
-            System.out.println("Encoded Text: " + encodedText);
-        } else {
-            String decodedText = decode(text, key); // Print result to string for decode instruction
-            System.out.println("Decoded Text: " + decodedText);
-        }
-    }
+    String instruction = args[0];
+    String text = args[1];
+    String key = args[2];
 
-    public static void main(String[] args) {
-        if (!validateInput(args)) {
-            return;
-        }
-
-        String instruction = args[0];
-        String text = args[1];
-        String key = args[2];
-
-        processInput(instruction, text, key);
-    }
+    processInput(instruction, text, key);
+  }
 }
