9f78ea42ef8ca3137d729867027757b38b234431
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-12 13:12:55 -0600

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,56 +1,54 @@
 /**
- * An implementation of the Caesar
- * Cipher. Implements the command line
- * for user input.
- * 
+ * An implementation of the Caesar Cipher. Implements the command line for user input.
+ *
  * @author Jonathan Wang
  * September 2023
  */
 public class CaesarCipher {
 
-    public static String encode(String plaintext, int key) {
-        char[] charArray = plaintext.toCharArray(); // Instantiate new charArray
+  public static String encode(String plaintext, int key) {
+    char[] charArray = plaintext.toCharArray(); // Instantiate new charArray
 
-        for (int i = 0; i < charArray.length; i++) { // Loop through charArray
-            char c = charArray[i]; // Set char as index of charArray
-            char encodedChar = (char) ((c - 'a' + key) % 26 + 'a'); // Convert c to unicode
-            charArray[i] = encodedChar; // set the unicoded char as the index value in charArray
-        }
-
-        return new String(charArray); // Return the ciphered charArray
+    for (int i = 0; i < charArray.length; i++) { // Loop through charArray
+      char c = charArray[i]; // Set char as index of charArray
+      char encodedChar = (char) ((c - 'a' + key) % 26 + 'a'); // Convert c to unicode
+      charArray[i] = encodedChar; // set the unicoded char as the index value in charArray
     }
 
-    public static String decode(String plaintext, int key) {
-        return encode(plaintext, 26 - key); // Call encode function with unicode to represent decode
+    return new String(charArray); // Return the ciphered charArray
+  }
+
+  public static String decode(String plaintext, int key) {
+    return encode(plaintext, 26 - key); // Call encode function with unicode to represent decode
+  }
+
+  public static boolean validateInput(String instruction, String input) {
+    if (!instruction.equals("encode") && !instruction.equals("decode")) { // Output error for invalid Instruction
+      System.err.println("Invalid instruction. Please use encode/decode");
+      return false;
     }
 
-    public static boolean validateInput(String instruction, String input) {
-        if (!instruction.equals("encode") && !instruction.equals("decode")) { // Output error for invalid Instruction
-            System.err.println("Invalid instruction. Please use encode/decode");
-            return false;
-        }
+    return true;
+  }
 
-        return true;
+  public static void processInput(String instruction, String input) {
+    for (int key = 0; key < 26; key++) { // Loop through and print every key value
+      String result = instruction.equals("encode") ? encode(input, key) : decode(input, key);
+      System.out.println("Key " + key + ": " + result);
+    }
+  }
+
+  public static void main(String[] args) {
+    if (args.length != 2) { // Checking for whitespaces
+      System.err.println("Invalid input. Please use a correct number of args.");
+      System.exit(2);
     }
 
-    public static void processInput(String instruction, String input) {
-        for (int key = 0; key < 26; key++) { // Loop through and print every key value
-            String result = instruction.equals("encode") ? encode(input, key) : decode(input, key);
-            System.out.println("Key " + key + ": " + result);
-        }
+    String instruction = args[0];
+    String input = args[1];
+
+    if (validateInput(instruction, input)) {
+      processInput(instruction, input); // Call functions to validate and process user input
     }
-
-    public static void main(String[] args) {
-        if (args.length != 2) { // Checking for whitespaces
-            System.err.println("Invalid input. Please use a correct number of args.");
-            System.exit(2);
-        }
-
-        String instruction = args[0];
-        String input = args[1];
-
-        if (validateInput(instruction, input)) {
-            processInput(instruction, input); // Call functions to validate and proccess user input
-        }
-    }
+  }
 }
